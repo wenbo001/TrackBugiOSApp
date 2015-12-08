66dcd316b6451ce08f749d7248631ec2a328ceef
wenbo001 <liuwenbocheer@163.com>
2015-12-08 18:37:28 +0100

    Initial Commit

---
+++ TrackBugApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrackBugApp
+//
+//  Created by wenbo on 12/8/15.
+//  Copyright © 2015 WenboLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TrackBugApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TrackBugApp
+//
+//  Created by wenbo on 12/8/15.
+//  Copyright © 2015 WenboLiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
